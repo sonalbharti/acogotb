6be6235fac2c30028655215228358f713d55f566
Aman Singh <bharti.amansingh@gmail.com>
2022-09-19 04:09:31 +0530

    Minor changes
    Added home icon.
    Reorganized the navigation bar.

--- src/SiteNavigationBar.js
+++ src/SiteNavigationBar.js
@@ -3,8 +3,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Image from 'react-bootstrap/Image';
 // import {LinkContainer} from 'react-router-bootstrap'
 import { useNavigate } from 'react-router-dom';
+import homeIcon from './home-icon.png'
 
 function SiteNavigationBar(props) {
     const navigate = useNavigate()
@@ -35,36 +37,25 @@ function SiteNavigationBar(props) {
 
     return (
         <Navbar className='navigation-bar' expand="lg" fixed="top">
-            <Container>
-                {/* <Navbar.Brand href="#home"><h1>A <i><b>Castle</b></i> of Glass on the Beach</h1></Navbar.Brand> */}
+                <Navbar.Brand className="navbar-home-icon" onClick={linkOnClick("home")} ><Image className="home-icon" src={homeIcon} /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link onClick={linkOnClick("home")}>HOME</Nav.Link>
-                    <NavDropdown className='navigation-dropdown' title="THE SERIES" id="basic-nav-dropdown">
+                {/* <Navbar.Collapse id="basic-navbar-nav"> */}
+                <Nav className="me-auto w-100 right-padding">
+                    {/* <Nav.Link onClick={linkOnClick("home")}><Image className="home-icon" src={homeIcon} /></Nav.Link> */}
+                    <Nav.Link onClick={linkOnClick("the-series-teleport")}>teleport</Nav.Link>
+                    <Nav.Link onClick={linkOnClick("the-series-rock-paper-scissors")}>rock-paper-scissors</Nav.Link>
+                    <Nav.Link onClick={linkOnClick("the-series-home")}>home</Nav.Link>
+                    <Nav.Link onClick={linkOnClick("the-series-alienation")}>alienation</Nav.Link>
+                    {/* <NavDropdown className='navigation-dropdown' title="THE SERIES" id="basic-nav-dropdown">
                     <NavDropdown.Item className='navigation-dropdown-item' onClick={linkOnClick("the-series-teleport")} >TELEPORT</NavDropdown.Item>
                     <NavDropdown.Item className='navigation-dropdown-item' onClick={linkOnClick("the-series-rock-paper-scissors")} >ROCK-PAPER-SCISSORS</NavDropdown.Item>
                     <NavDropdown.Item className='navigation-dropdown-item' onClick={linkOnClick("the-series-home")} >HOME</NavDropdown.Item>
-                    {/* <NavDropdown.Item className='navigation-dropdown-item' href="#the-series/alienation" onClick={props.showAlienationPage} >ALIENATION</NavDropdown.Item> */}
                     <NavDropdown.Item className='navigation-dropdown-item' onClick={linkOnClick("the-series-alienation")} >ALIENATION</NavDropdown.Item>
-                    {/* <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">
-                        Separated link
-                    </NavDropdown.Item> */}
-                    </NavDropdown>
-                    {/* <NavDropdown className='navigation-dropdown' title="THE CITIES" id="basic-nav-dropdown">
-                    <NavDropdown.Item className='navigation-dropdown-item' href="#the-cities/jaisalmer">JAISALMER</NavDropdown.Item>
-                    <NavDropdown.Item className='navigation-dropdown-item' href="#the-cities/naggar">NAGGAR</NavDropdown.Item>
-                    <NavDropdown.Item className='navigation-dropdown-item' href="#the-cities/new-delhi">NEW DELHI</NavDropdown.Item>
-                    <NavDropdown.Item className='navigation-dropdown-item' href="#the-cities/bhopal">BHOPAL</NavDropdown.Item>
-                    <NavDropdown.Item className='navigation-dropdown-item' href="#the-cities/gangtok">GANGTOK</NavDropdown.Item>
-                    <NavDropdown.Item className='navigation-dropdown-item' href="#the-cities/kochi">KOCHI</NavDropdown.Item>
                     </NavDropdown> */}
-                    <Nav.Link onClick={linkOnClick("the-cities")} >THE CITIES</Nav.Link>
+                    <Nav.Link className="push-right" onClick={linkOnClick("the-cities")} ><b>THE CITIES</b></Nav.Link>
                     <Nav.Link onClick={linkOnClick("about")} >ABOUT</Nav.Link>
                 </Nav>
-                </Navbar.Collapse>
-            </Container>
+                {/* </Navbar.Collapse> */}
         </Navbar>
     )
 }
